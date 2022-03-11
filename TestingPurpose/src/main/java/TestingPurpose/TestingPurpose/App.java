94f4b038ba6c49dd5cf098312414cb743f2908d5
MAHESH KUMAR <MAHESH KUMAR@DESKTOP-KTPP931>
2022-03-11 11:38:06 +0530

    this is my first commit

---
+++ TestingPurpose/src/main/java/TestingPurpose/TestingPurpose/App.java
@@ -0,0 +1,13 @@
+package TestingPurpose.TestingPurpose;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
